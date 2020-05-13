7a836e6cac05f67a81d7b188d35e977f7f37930a
AlekseyAndrianov <43870626+AlekseyAndrianov@users.noreply.github.com>
2020-05-13 12:46:29 +0300

    KYC-43: Exclude env variable initialization

--- indexer/src/main/java/ru/newbank/zakupki/indexer/IndexerApplication.java
+++ indexer/src/main/java/ru/newbank/zakupki/indexer/IndexerApplication.java
@@ -5,14 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IndexerApplication {
-
-    static {
-        System.setProperty("ZAKUPKI_DB_URL", "jdbc:postgresql://localhost:5432/zakupki");
-        System.setProperty("ZAKUPKI_DB_USERNAME", "postgres");
-        System.setProperty("ZAKUPKI_DB_PASSWORD", "1234");
-        System.setProperty("ROOT_URL", "D:/Projects/Banking/");
-
-    }
+//
+//    static {
+//        System.setProperty("ZAKUPKI_DB_URL", "jdbc:postgresql://localhost:5432/zakupki");
+//        System.setProperty("ZAKUPKI_DB_USERNAME", "postgres");
+//        System.setProperty("ZAKUPKI_DB_PASSWORD", "1234");
+//        System.setProperty("ROOT_URL", "D:/Projects/Banking/");
+//
+//    }
     public static void main(String[] args) {
 		SpringApplication.run(IndexerApplication.class, args);
     }
